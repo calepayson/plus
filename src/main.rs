4981acc2cd8cfbe35752c45cbe37174cb3f7534d
Cale Smith <calepayson@mac.com>
2024-03-05 17:44:10 -0800

    Extracted the path to a function

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let path_buff: PathBuf = env::current_dir().unwrap();
-
-    let path = path_buff.to_str().expect("error"); 
+    let path = get_current_path();
 
     println!("The current directory is: {path}");
+}
+
+fn get_current_path() -> String {
+    let path_buff: PathBuf = env::current_dir().unwrap();
+
+    path_buff.to_str().unwrap().to_string()
 }
