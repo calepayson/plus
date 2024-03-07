bfe426dd24af7b250aa4dba018441ca8a970305b
Cale Smith <calepayson@mac.com>
2024-03-07 11:11:14 -0800

    Successfully prints current working path

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
-    let path = get_current_path();
+    let path = match env::current_dir() {
+        Ok(path) => path,
+        Err(e) => panic!("Could not get current working path: {e}"),
+    };
 
-    println!("The current directory is: {path}");
-}
-
-fn get_current_path() -> String {
-    let path_buff: PathBuf = env::current_dir().unwrap();
-
-    path_buff.to_str().unwrap().to_string()
+    println!("The current directory is: {:?}", path);
 }
