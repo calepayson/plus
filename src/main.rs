98b76cfdd5693d36e6332e97d234682c6a30303b
Cale Smith <calepayson@mac.com>
2024-03-07 13:02:51 -0800

    Prints current directory and path

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
     let path = match env::current_dir() {
         Ok(path) => path,
         Err(e) => panic!("Could not get current working path: {e}"),
     };
+    
+    print_directory_contents(path, 0);
+}
 
-    let dir_contents = match fs::read_dir(path) {
+fn print_directory_contents(path: PathBuf, depth: u32) {
+    println!("{:?}", path.file_name().unwrap());
+
+    let directory_contents = match fs::read_dir(path) {
         Ok(contents) => contents,
         Err(e) => panic!("Could not read current directory: {e}"),
     };
 
-    for item in dir_contents {
+    for item in directory_contents {
         match item {
-            Ok(i) => println!("=> {:#?}", i),
+            Ok(i) => println!("{:?} at depth: {}", i.path(), depth),
             Err(e) => println!("Could not read item: {e}"),
         }
     }
 }
+
