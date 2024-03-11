05ef035500bd2498fdcfae6f041fa0efcfdd059f
Cale Smith <calepayson@mac.com>
2024-03-11 16:10:53 -0700

    Wrote a formatting function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 use ignore::DirEntry;
+use std::format;
 
 pub fn pretty_print_dir_entry(entry: DirEntry) {
-    println!("{}", entry.path().display());
+    println!("{}{}", get_formatting_chars(&entry), entry.path().display());
+}
+
+pub fn get_formatting_chars(entry: &DirEntry) -> String {
+    let depth = entry.depth();
+    let spaces = " ".repeat(depth * 4);
+    let delimeter = "-";
+
+    format!("{}{}", spaces, delimeter)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn tests_work() {
+        assert_eq!(1 + 1, 2);
+    }
 }
